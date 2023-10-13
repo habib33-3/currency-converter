c54070c2804218e44c1a2c386a14ade12ef7ff8c
Habibur Rahman <habib.swi16@gmail.com>
2023-10-13 16:42:45 +0600

    ui and prop

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 
+import { useId } from "react";
+
 const InputBox = ({
   label,
   amount,
@@ -11,11 +13,18 @@ const InputBox = ({
   currencyDisable = false,
   className = "",
 }) => {
+  const amountInputId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label
+          className="text-black/40 mb-2 inline-block"
+          htmlFor={amountInputId}
+        >
+          {label}
+        </label>
         <input
+        id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
